07141caa44065de48fd1a1ce388235d0546d8a32
abisdbest <105310709+abisdbest@users.noreply.github.com>
2024-06-19 13:36:55 +0100

    hi

--- run/run.js
+++ run/run.js
@@ -1,3 +1,4 @@
+var html = '';
 async function fetchText(filename) {
     try {
         const url = `https://quizizzfilehost.blaub002-302.workers.dev/${filename}?nocache=${new Date().getTime()}`;
@@ -13,31 +14,15 @@ async function fetchText(filename) {
     }
 }
 
-// Function to fetch and replace head content
-async function replaceHead() {
-    var head = await fetchText("head.html");
-    if (head !== null) { // Check if head is not null before replacing
-        document.head.innerHTML = head;
-    } else {
-        console.error("Failed to fetch head content");
-    }
+// Function to fetch and write HTML
+async function init_index() {
+    html = await fetchText("index.html");
+    html = html.replace('src="script.js"', 'src="https://quizizzfilehost.blaub002-302.workers.dev/script.js"');
+    html = html.replace('href="style.css"', 'href="https://quizizzfilehost.blaub002-302.workers.dev/style.css"');
+    html = html.replace('src="image.png"', 'src="https://quizizzfilehost.blaub002-302.workers.dev/image.png"');
+    html = html.replace('src="folder/script.js"', 'src="https://quizizzfilehost.blaub002-302.workers.dev/folder/script.js"');
+    html = html.replace('href="folder/style.css"', 'href="https://quizizzfilehost.blaub002-302.workers.dev/folder/style.css"');
+    document.write(html);
 }
 
-// Function to fetch and replace body content
-async function replaceBody() {
-    var body = await fetchText("body.html");
-    if (body !== null) { // Check if body is not null before replacing
-        body = body.replace('src="script.js"', 'src="https://quizizzfilehost.blaub002-302.workers.dev/script.js"');
-        body = body.replace('href="style.css"', 'href="https://quizizzfilehost.blaub002-302.workers.dev/style.css"');
-        body = body.replace('src="image.png"', 'src="https://quizizzfilehost.blaub002-302.workers.dev/image.png"');
-        body = body.replace('src="folder/script.js"', 'src="https://quizizzfilehost.blaub002-302.workers.dev/folder/script.js"');
-        body = body.replace('href="folder/style.css"', 'href="https://quizizzfilehost.blaub002-302.workers.dev/folder/style.css"');
-        document.body.innerHTML = body;
-    } else {
-        console.error("Failed to fetch body content");
-    }
-}
-
-// Call the functions to replace head and body content
-replaceHead();
-replaceBody();
+init_index();
